8c2565c083d4eaf1a0b7ab461792146b03605844
NIhalrokzz <singhnihal838@gmail.com>
2022-03-14 20:11:37 +0530

    Adding new Address files

---
+++ composite-service/src/main/java/com/example/composite_service/dto/CustomerBillingAddress.java
@@ -0,0 +1,88 @@
+package com.example.composite_service.dto;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer_billing_address")
+public class CustomerBillingAddress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private Long DoorNo;
+    private String streetName;
+    private String Layout;
+    private String city;
+    private int pincode;
+
+	public CustomerBillingAddress() {
+	}
+
+	public CustomerBillingAddress(Long doorNo, String streetName, String layout, String city, int pincode) {
+		DoorNo = doorNo;
+		this.streetName = streetName;
+		Layout = layout;
+		this.city = city;
+		this.pincode = pincode;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Long getDoorNo() {
+		return DoorNo;
+	}
+
+	public void setDoorNo(Long doorNo) {
+		DoorNo = doorNo;
+	}
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+	public String getLayout() {
+		return Layout;
+	}
+
+	public void setLayout(String layout) {
+		Layout = layout;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+
+	@Override
+	public String toString() {
+		return "CustomerAddress [id=" + id + ", DoorNo=" + DoorNo + ", streetName=" + streetName + ", Layout=" + Layout
+				+ ", city=" + city + ", pincode=" + pincode + "]";
+	}
+	
+	
+	
+	
+    
+    
+}
